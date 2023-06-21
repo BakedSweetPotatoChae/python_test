03d9b3665a74d8215076a95613aef29d71ac624f
BakedSweetPotatoChae <byung10040@naver.com>
2023-06-21 14:13:15 +0900

    update : 단어공부

--- 심화_1/단어공부.py
+++ 심화_1/단어공부.py
@@ -1,3 +1,31 @@
 text_input = str(input())
-text_input=text_input.lower()
-print(text_input)
+text_input=text_input.upper()
+
+
+text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+text_save_number = [0 for i in range(26)]
+
+
+
+for i in range(len(text_input)):
+    text_save_number[text.find(text_input[i])] += 1
+
+
+max = int(0)
+max_check = int(0)
+for i in range(26):
+    if(max <= text_save_number[i]):
+        max = text_save_number[i]
+        max_len = i
+
+
+duplication = False
+for i in range(26):
+    if(max == text_save_number[i] and max_len != i):
+        duplication = True
+
+
+if(duplication == True):
+    print("?")
+else:
+    print(text[max_len])
