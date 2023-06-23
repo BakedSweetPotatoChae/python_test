5c4ca553c1dd4297e171461a0dcafb2e8a258bce
BakedSweetPotatoChae <byung10040@naver.com>
2023-06-24 02:30:56 +0900

    add:행렬덧셈.py

--- 2차원 배열/행렬 덧셈.py
+++ 2차원 배열/행렬 덧셈.py
@@ -1,9 +1,8 @@
 number, number_2 = map(int, input().split())
-#print(number, number_2, sep="\t")
 
 list_number = []
 list_number_2 = []
-list_number_sum = [[0 for i in range(number)] for i in range(number_2)]
+list_number_sum = [[0 for i in range(number_2)] for i in range(number)]
 
 for i in range(number):
     input_number = input()
@@ -15,18 +14,12 @@ for i in range(number):
         
 for i in range(number):
     for j in range(number_2):
-        #print("{}+{}".format(int(list_number[i][j]),int(list_number[i][j])))
+        #print("i:{} j:{}, list_number : {} list_number_2 : {}".format(i,j,list_number[i][j],list_number_2[i][j]))
         list_number_sum[i][j] = int(list_number[i][j]) + int(list_number_2[i][j])
 
-'''
-print(list_number)
-
-print(list_number_2)
-
-print(list_number_sum)
-'''
 
 for i in range(number):
-    for j in range(number_2):
+    for j in range(number_2-1):
         print(list_number_sum[i][j], end=" ")
+    print(list_number_sum[i][number_2-1], end="")
     print()
